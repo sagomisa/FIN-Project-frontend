ea1ef6b5d2434aa127d09a5b72784e869418e38f
Nisha-Bhattarai <c0840054@mylambton.ca>
2023-04-14 17:06:38 -0400

    Created gallery and added images

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import Sidebar from '../../components/sidebar/Sidebar'
+import React from "react";
+import GalleryComponent from "../../components/galleryComponent/GalleryComponent";
+import Sidebar from "../../components/sidebar/Sidebar";
 
 const Gallery = () => {
   return (
     <div className="dashboard">
       <Sidebar />
       <div className="dashboard-content">
-        <h1>Welcome to Gallery Page!</h1>
+        <h1 className="--text-center">Welcome to our Gallery Page!</h1>
+        <p className="--text-center --mb">
+          Here you can find a collection of photos featuring our team members
+          and some of our memorable meetings and events.
+        </p>
+        <GalleryComponent />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Gallery
+export default Gallery;
